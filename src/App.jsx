ff7e2c5c841fe785986f7fef1aba0a79a9bae3bd
KingKyoTrue <tanadicoding@gmail.com>
2023-08-04 13:39:28 +0700

    deleted contact from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Navbar from './components/Navbar';
 import { Route, Routes } from "react-router-dom"
 import BlogPage from './pages/BlogPage';
 import ProjectPage from './pages/ProjectPage';
-import ContactPage from './pages/ContactPage';
 import NotFound from './pages/NotFound';
 import SinglePost from './pages/SinglePost';
 
@@ -20,7 +19,6 @@ function App() {
         <Route path="/blog/:slug" Component={SinglePost}/>
         <Route path="/blog" Component={BlogPage}/>
         <Route path="/project" Component={ProjectPage}/>
-        <Route path="/contact" Component={ContactPage}/>
         <Route path='*' Component={NotFound}/>
     </Routes>
 
